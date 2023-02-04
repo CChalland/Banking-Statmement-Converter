905013b35fb6cab7d8b6dbee16f2d8e3c9b787aa
Cole Challand <ctchalland@gmail.com>
2023-02-04 07:14:13 -0500

    added excel file to mess around with

--- main.py
+++ main.py
@@ -6,8 +6,13 @@ from card_filter import StatementFilter
 
 
 if __name__ == "__main__":
-    statements = StatementFilter()
-    statements.crawl_directory("Statements")
+    statements = StatementFilter("Statements")
+    statements.crawl_directory()
     data = statements.results
-    
     print("\nALL DATA: ", data)
+    
+    # for file in statements.input_files:
+    #     print("file: ", file)
+    #     data = statements._data_praser(file)
+    #     print("data = ", data, "\n\n")
+    
